4a6cb6d92e2d7fb788fb60e6b4366895d0a95876
Luke Davison <luke.davison@gmail.com>
2010-11-12 16:40:02 -0700

    initial import

---
+++ test.class.php
@@ -0,0 +1,33 @@
+<?php
+
+class Testing
+{
+	private $name;
+	private $value;
+	
+	protected function getName()
+	{
+		return $this->name;
+	}
+	
+	protected function setName($name)
+	{
+		$this->name = $name;
+	}
+	
+	protected function getValue()
+	{
+		return $this->value;
+	}
+	
+	protected function setValue($value)
+	{
+		$this->value = $value;
+	}
+	
+	function __construct($name='', $value='')
+	{
+		$this->name = $name;
+		$this->value = $value;
+	}
+}
